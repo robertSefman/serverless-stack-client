29b87ff55f7f44d9b32acf10a9ccc4f338db7ec2
Robert Šefman <robert@epodjetnik.com>
2020-05-20 19:57:17 +0200

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload-166",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: " https://ryovize8s7.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_E1rBhWRZD",
+    APP_CLIENT_ID: "57gm05n423rmp7mjvsfcb3kgid",
+    IDENTITY_POOL_ID: "us-east-1:3676dadb-4244-4fc4-b4b2-22cdeebba165",
+  },
+}
