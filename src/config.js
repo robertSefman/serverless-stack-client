90da220f86959f0ce8bd431f33ce49a1828f675b
Robert Šefman <robert@epodjetnik.com>
2020-05-21 12:29:02 +0200

    Building App

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_kt0azGnyVtnNXZTzC9LCzQg400mzoSON0P",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-upload-166",
